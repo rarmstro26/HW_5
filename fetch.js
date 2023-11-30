616663db434e1f6e25f11f535ef80b4ba1d8ab3b
Ryan Armstrong <rarmstro@bu.edu>
2023-11-30 17:23:47 -0500

    v1

---
+++ fetch.js
@@ -0,0 +1,22 @@
+
+// Wait for window load - setup click here button to launch fetch function
+window.onload = function() {
+    let clickData = document.getElementById('main-button');
+    // Button, when clicked, run getData function
+    clickData.onclick = getData;
+}
+
+// Fetch function to retrieve/handle response and display educationd ata
+function getData(){
+    alert("Function started!");
+    console.log("Function started");
+
+// Create a fetch request to return a promise
+
+// Resolve the promise using the Response class
+
+// Check the status code of the response
+
+// Process the returned JSON data using JS (DOM)
+
+}
